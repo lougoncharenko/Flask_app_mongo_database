027ad84b12ddb69057d1906eb4bb6ca68131c26d
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-01 13:38:26 -0500

    created a route to handle form dta

--- app.py
+++ app.py
@@ -14,6 +14,17 @@ todos = db.todos
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method=='POST':
+        content = request.form['content']
+        priority = request.form['priority']
+        todos.insert_one({'content': content, 'priority': priority})
+        return redirect(url_for('index'))
+
+    all_todos = todos.find()
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port = 3000)
