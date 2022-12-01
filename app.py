35083c62c6b91167491ba8e6abc2f54cbbf1c3f6
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-01 13:16:57 -0500

    added a port

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ todos = db.todos
 @app.route('/', methods=('GET', 'POST'))
 def index():
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 3000)
